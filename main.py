8933703a2b180b0b3fd9f2d97b334770cd6663f5
Marius <Maariius@outlook.de>
2024-03-22 03:58:32 +0100

    started implementing login

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from flask import Flask, jsonify, request
 from flask_socketio import SocketIO, send, emit
+from flask_cors import CORS, cross_origin
+import bcrypt
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+cors = CORS(app)
+#app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 messages = []
 
+# Route for login
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json() # User data from request
+
+    # check if username and password exist and are correct
+    #  if ... DB CODE GEDÖNS
+    # return jsonify({'success': True, 'message': 'Login successful'})
+    # else
+    # return jsonify({'success': False, 'message': 'wrong credentials'}), 401
+
 # Route for receiving messages via HTTP POST
 @app.route('/messages', methods=['POST'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def receive_messages():
     data = request.get_json()
     message = {
@@ -21,6 +38,7 @@ def receive_messages():
 
 #Route zum Abrufen von Nachrichten über HTTP GET
 @app.route('/messages', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def get_messages():
     return jsonify(messages)
 
