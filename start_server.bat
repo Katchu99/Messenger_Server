e90ce5535a984d6a4bf5a1695540666a1ac6e23a
Marius <Maariius@outlook.de>
2024-06-01 15:01:03 +0200

    simple start server batch script

---
+++ start_server.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\Maari\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\python3.exe" "C:\Users\Maari\Desktop\Project\Messenger_Server\server.py"
+pause
