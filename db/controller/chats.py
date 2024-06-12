97ab607db617894b2d3a83f048aec640c231da27
Marius <Maariius@outlook.de>
2024-06-12 23:20:23 +0200

    mongodb

--- db/controller/chats.py
+++ db/controller/chats.py
@@ -1,17 +1,8 @@
 import sys
 sys.path.append("..")  # Fügt das übergeordnete Verzeichnis dem Python-Systempfad hinzu
 
-from index import mongo_obj
+from db.chats import mongo_obj
 mongo_db = mongo_obj.mongo_db
-
-
-""" chat_data = {
-        "messages": [
-            {"_id":  ObjectId() ,"userid":"user1", "message": "Hello", "timestamp": "2024-06-09 12:00:00"},
-            {"_id":  ObjectId() ,"userid":"user2", "message": "Hi there", "timestamp": "2024-06-09 12:05:00"}
-        ]
-    }    
-insert_chat_to_db(chat_data) """
 
 def insert_chat_to_db():
     chat_data = {
